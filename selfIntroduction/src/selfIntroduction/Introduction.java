b4629ebb5cfd367c5b06a06fc0e1d224192f10fb
vrishabhpatil <90345728+vrishabhpatil@users.noreply.github.com>
2021-09-10 00:27:40 +0530

    push to cvcm
    self

---
+++ selfIntroduction/src/selfIntroduction/Introduction.java
@@ -0,0 +1,15 @@
+package selfIntroduction;
+
+public class Introduction {
+public static void main (String[]args) {
+	System.out.println("Name-Vrishabh Ravindra Patil");
+	System.out.println("Education-B.E.Civil Engineering");
+	System.out.println("College-PRMCEAM BADNERA");
+	System.out.println("Address-Gadge lay out pandharkawada dist. yavatmal");
+	System.out.println("pin code-445302");
+	System.out.println("Mob No.-8623800650");
+	System.out.println("Email-vrishabhpatil0106@gmail.com");
+	System.out.println(1234567890);
+	
+}
+}
---
+++ selfIntroduction/src/selfIntroduction/Secondprogram.java
@@ -0,0 +1,10 @@
+package selfIntroduction;
+
+public class Secondprogram {
+
+public static void main(String[] args) {
+System.out.print("Om Namh Shivay"+ "\n" + "Name: Bala" + "\n" +  "123" +  "\n");
+System.out.println("Aqw");
+	}
+
+}
