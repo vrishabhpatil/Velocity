31b6a34f784c1a23f4bf5e5931f135516ecbb7b0
vrishabhpatil <90345728+vrishabhpatil@users.noreply.github.com>
2021-10-02 22:57:16 +0530

    Update EXcelsheetHANDLE.java

--- May21_Evening/src/seleniumPRACTICE/EXcelsheetHANDLE.java
+++ May21_Evening/src/seleniumPRACTICE/EXcelsheetHANDLE.java
@@ -11,10 +11,14 @@ public class EXcelsheetHANDLE {
 	
 	public void excel() throws EncryptedDocumentException, IOException {
          String path ="G:\\velocity class\\ExcelsheetHANDLE.xlsx";
+         for (int i=0;i<=3;i++) {
+        	 for(int j=0;j<2;j++) {
          FileInputStream f= new FileInputStream(path);
-         String val = WorkbookFactory.create(f).getSheet("Sheet1").getRow(0).getCell(0).getStringCellValue();
-         System.out.println(val);
-         
+         String val = WorkbookFactory.create(f).getSheet("Sheet1").getRow(i).getCell(j).getStringCellValue();
+          System.out.print("  "+val);
+        	 }
+        	 System.out.println();
+         }
 }
 	public static void main(String[] args) throws EncryptedDocumentException, IOException {
 		EXcelsheetHANDLE x= new EXcelsheetHANDLE();
